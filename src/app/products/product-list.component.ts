e51e159e7c5814df88597cac3ba4e2f988e75c45
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-04-28 20:59:16 +0100

    [Product] Add filter to list of product
    Products list cannot be filtered by search
    User can search through list of products for desired product
    Created a template refernce variable in the parent component
    to access the methods and properties in the child component
    Parent component: <ProductListComponent>
    Child component:  <CriteriaComponent>
    
    [Finishes]

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,62 +1,79 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import { CriteriaComponent } from '../shared/criteria/criteria.component';
 
 @Component({
-    templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
-    pageTitle: string = 'Product List';
-    showImage: boolean;
+export class ProductListComponent implements OnInit, AfterViewInit {
+  pageTitle: string = 'Product List';
+  showImage: boolean;
 
-    includeDetail: boolean = true;
+  includeDetail: boolean = true;
 
-    imageWidth: number = 50;
-    imageMargin: number = 2;
-    errorMessage: string;
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  errorMessage: string;
 
-    // Using Setter and Getter
+  @ViewChild(CriteriaComponent) filterComponent: CriteriaComponent;
+  parentListFilter: string;
 
-    // // create a private property
-    // private _listFilter: string;
+  // Using Setter and Getter
 
-    // // create a Getter propery
-    // get listFilter(): string {
-    //   return this._listFilter;
-    // }
+  // // create a private property
+  // private _listFilter: string;
 
-    // // create a Setter property
-    // set listFilter(valueToSet: string) {
-    //   this._listFilter = valueToSet;
-    //   this.performFilter(this.listFilter);
-    // }
+  // // create a Getter propery
+  // get listFilter(): string {
+  //   return this._listFilter;
+  // }
 
-    filteredProducts: IProduct[];
-    products: IProduct[];
+  // // create a Setter property
+  // set listFilter(valueToSet: string) {
+  //   this._listFilter = valueToSet;
+  //   this.performFilter(this.listFilter);
+  // }
 
-    constructor(private productService: ProductService) { }
+  filteredProducts: IProduct[];
+  products: IProduct[];
 
-    ngOnInit(): void {
-        this.productService.getProducts().subscribe(
-            (products: IProduct[]) => {
-                this.products = products;
-                this.performFilter();
-            },
-            (error: any) => this.errorMessage = <any>error
-        );
+  constructor(private productService: ProductService) { }
+
+
+  ngAfterViewInit(): void {
+    if (this.filterComponent.listFilter) {
+      this.parentListFilter = this.filterComponent.listFilter;
+      console.log(this.parentListFilter);
+    }
+  }
+
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(
+      (products: IProduct[]) => {
+        this.products = products;
+        this.performFilter(this.parentListFilter);
+      },
+      (error: any) => this.errorMessage = <any>error
+    );
+  }
+
+
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy?: string): void {
+    if (filterBy) {
+      this.filteredProducts = this.products.filter((product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy.toLocaleLowerCase()) !== -1);
+    } else {
+      this.filteredProducts = this.products;
     }
 
-    toggleImage(): void {
-        this.showImage = !this.showImage;
-    }
-
-    performFilter(filterBy?: string): void {
-        if (filterBy) {
-            this.filteredProducts = this.products.filter((product: IProduct) =>
-                product.productName.toLocaleLowerCase().indexOf(filterBy.toLocaleLowerCase()) !== -1);
-        }
-          this.filteredProducts = this.products;
-    }
+  }
 }
